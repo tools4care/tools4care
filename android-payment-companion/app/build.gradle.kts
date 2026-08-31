5681bbf565931ee12c8a0539c94a975763a09a62
tools4care <edwin_evangelista@hotmail.com>
2026-08-31 08:43:53 -0400

    Build Tap to Pay companion visual update

--- android-payment-companion/app/build.gradle.kts
+++ android-payment-companion/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.tools4care.payments"
         minSdk = 30
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.2.1-pilot"
+        versionCode = 7
+        versionName = "0.2.2-pilot"
 
         buildConfigField("String", "SUPABASE_URL", "\"$supabaseUrl\"")
         buildConfigField("String", "SUPABASE_ANON_KEY", "\"$supabaseAnonKey\"")
